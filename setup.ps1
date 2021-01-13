500dd51398e37d83ff1294d1445e0d49b0c02f5f
Jefferson Ding <tyding2004@outlook.com>
2021-01-12 23:01:35 -0500

    updateO

--- setup.ps1
+++ setup.ps1
@@ -7,6 +7,6 @@ Set-Location tmp
 mkdir backup
 mkdir undo
 Set-Location ../autoload
-Invoke-WebRequest https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -O p
+Invoke-WebRequest https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -O plug.vim
 Set-Location ..
 Write-Output "let g:python_host_prog='C:/Users/tydin/AppData/Local/Programs/Python/Python39/python.exe'" | out-file "_machine_specific.vim" -encoding utf8
