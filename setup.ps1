93a1ce9799b04ca05c2f50200cf82b58a6309d8b
Jefferson Ding <tyding2004@outlook.com>
2021-01-12 22:53:58 -0500

    windows setup

---
+++ setup.ps1
@@ -0,0 +1,12 @@
+cmd /c "mklink /D  %HOMEPATH%\.config\nvim %HOMEPATH%\AppData\Local\nvim"
+Set-Location $HOME/.config/nvim
+mkdir autoload
+mkdir plugged
+mkdir tmp
+Set-Location tmp
+mkdir backup
+mkdir undo
+Set-Location ../autoload
+Invoke-WebRequest https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -O p
+Set-Location ..
+Write-Output "let g:python_host_prog='C:/Users/tydin/AppData/Local/Programs/Python/Python39/python.exe'" | out-file "_machine_specific.vim" -encoding utf8
